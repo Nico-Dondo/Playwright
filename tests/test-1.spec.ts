ee67e822f6e4cfda453a4c04ee38c221215cf466
Nicolas <nicodondo1980@gmail.com>
2024-01-15 22:39:29 -0300

    creacion de mas casos de prueba

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,4 +1,4 @@
-import { test, Browser, Page } from '@playwright/test';
+import { test, Browser, Page, expect } from '@playwright/test';
 
 (async () => {
     let browser: Browser;
@@ -8,7 +8,16 @@ import { test, Browser, Page } from '@playwright/test';
         test('Los Links principales redirigen correctamente', async ({ page }) => {
             await test.step('Estando yo en la web principal de www.freerangetesters.com', async () => {
                 await page.goto('https://www.freerangetesters.com');
+                await expect(page).toHaveTitle('Free Range Testers');
             });
+            await test.step('Cuando hago click en cursos', async () => {
+                page.locator('#page_header').getByRole('link', { name: 'Cursos', exact: true }).click();
+                await page.waitForURL('**/cursos');
+            })
+            await test.step('Soy redirigido a la subpagina "cursos"', async () => {
+                await expect(page).toHaveTitle('Cursos');
+
+            })
         });
     });
 })();
