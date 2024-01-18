6c1cc8e75d75086f7eb8e6362a3394d1013c638d
Nicolas <nicodondo1980@gmail.com>
2024-01-17 23:44:02 -0300

    Correcciones

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -4,36 +4,30 @@ import { test, Browser, Page, expect } from '@playwright/test';
   let browser: Browser;
   let page: Page;
 
-  test.describe('Navegación en www.freerangetesters.com', () => {
-
+  test.describe('Navegacion en www.freerangetesters.com',()=>{
     const secciones = [
-      { nombre: 'Cursos', url: '/cursos', tituloEsperado: 'Cursos' },
-      { nombre: 'Udemy', url: '/udemy', tituloEsperado: 'Udemy' },
-      { nombre: 'Recursos', url: '/recursos', tituloEsperado: 'Recursos' },
-      { nombre: 'Blog', url: '/login', tituloEsperado: 'Acceder a Free Range Testers' }
-      // Agrega más secciones si es necesario
+      { nombre: 'Cursos', url: '/cursos', tituloEsperado: 'Cursos' }
+,     {nombre : 'Udemy', url:'/udemy', tituloEsperado: 'Udemy'},
+      {nombre : 'Recursos', url:'/recursos', tituloEsperado: 'Recursos'}, 
+      
     ];
-    for (const seccion of secciones) { 
-        test(`Validar redirección a la sección "${seccion.nombre}"`, async ({ page }) => {
-         await test.step(`Estando yo en la web principal www.freerangetesters.com`, async () => {
+    for ( const seccion of secciones){
+      test(`Validar redireccion a la seccion "${seccion.nombre}"`, async ({page})=>{
+        await test.step(`Estando yo en la web principal www.freerangetesters.com`, async ()=>{
           page.goto('https://www.freerangetesters.com');
           await expect(page).toHaveTitle('Free Range Testers');
         });
-
-        await test.step(`Cuando hago click en "${seccion.nombre}"`, async () => {
-          page.locator('#page_header').getByRole('link', { name: seccion.nombre, exact: true }).click();
-          await page.waitForURL(`${seccion.url}`);
+        await test.step (`Cuando hago click en "${seccion.nombre}"`, async ()=>{
+          page.locator('#page_header').getByRole ('link', {name: seccion.nombre, exact: true}).click();
+          await page.waitForURL(`**${seccion.url}`);
         });
-
-        await test.step(`Soy redirigido a la sección de título "${seccion.tituloEsperado}"`, async () => {
-          await expect(page).toHaveTitle(seccion.tituloEsperado);
-
-        
+        await test.step(`Soy redirigido a la seccion de titulo "${seccion.tituloEsperado}"`, async ()=>{
+          await expect (page).toHaveTitle(seccion.tituloEsperado);
         });
       });
     }
-
   })
+ 
 
 
 })(); 
