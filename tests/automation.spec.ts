1e953ec028d5d63ad6b9a1f4ccb1dca3aa4389e6
Nicolas <nicodondo1980@gmail.com>
2024-02-08 00:38:46 -0300

    Utilizacion de Locators

--- tests/automation.spec.ts
+++ tests/automation.spec.ts
@@ -7,13 +7,27 @@ import { test, Browser, Page, expect } from '@playwright/test';
 
     test.describe('Acciones en el  Automation Sandbox', () => {
         test('Click en Botn ID Dinamico', async ({ page }) => {
-            await test.step('Dado que avego al sandbox de Free Range Testers', async () => {
-                await page.goto('http://localhost:3000/sandbox-automation-testing')
+            await test.step('Dado que navego al sandbox de Free Range Testers', async () => {
+                await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/')
+            })
+
+            await test.step('Puedo hacer click en el boton ID dinamico', async () => {
+                await page.getByRole('button', { name: 'Hacé click para generar un ID' }).click();
+
             })
 
 
+        })
+        test('lleno un campo de texto en Automation Sandbox', async ({ page }) => {
+            await test.step('Dado que navego al sandbox de Free Range Testers', async () => {
+                await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/')
+            })
+            await test.step('Puedo ingresar un texto en el campo un aburrido texto', async () => {
+                await page.getByPlaceholder('Ingresá texto').fill('Estoy aprendiendo Playwright')
+            })
 
         })
 
     })
+
 })();
